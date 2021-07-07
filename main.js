fbb7ff9142d060c819bc0a2a5b5f7fa4aa736340
JK-WLDC <68417907+JK-WLDC@users.noreply.github.com>
2021-07-07 19:11:57 +0100

    getting location of ISS and refreshing page every 5 seconds

--- main.js
+++ main.js
@@ -4,7 +4,21 @@ const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 async function getISS() {
 	const response = await fetch(api_url);
 	const data = await response.json();
-	console.log(data);
+	// by reference from json =
+	//console.log(data.latitude);
+	//console.log(data.longitude);
+	//
+	// below = seperates variables
+	const { latitude, longitude } = data;
+	//console.log(latitude);
+	//console.log(longitude);
+	//display variables
+	document.getElementById('lat').textContent = latitude;
+	document.getElementById('lon').textContent = longitude;
 }
 
 getISS();
+
+setTimeout(function () {
+	window.location.reload(1);
+}, 5000);
