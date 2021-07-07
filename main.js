6dcf4ee60bd1ceebdd514be081b25a306419e970
JK-WLDC <68417907+JK-WLDC@users.noreply.github.com>
2021-07-07 18:59:32 +0100

    json from api coming in

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('hello');
+//console.log('hello');
+
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+async function getISS() {
+	const response = await fetch(api_url);
+	const data = await response.json();
+	console.log(data);
+}
+
+getISS();
