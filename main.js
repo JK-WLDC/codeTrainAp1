92e9f988f2cdba8d18fcaaea885a6ff949c3934d
JK-WLDC <68417907+JK-WLDC@users.noreply.github.com>
2021-07-08 10:28:50 +0100

    ISS location marked on map which updates

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 //console.log('hello');
+const mymap = L.map('mapid').setView([0, 0], 1);
+const marker = L.marker([0, 0]).addTo(mymap);
+const attribution =
+	'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+const tiles = L.tileLayer(tileUrl, { attribution });
+
+tiles.addTo(mymap);
 
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+let firstTime = true;
+
 async function getISS() {
 	const response = await fetch(api_url);
 	const data = await response.json();
@@ -10,17 +23,20 @@ async function getISS() {
 	//
 	// below = seperates variables
 	const { latitude, longitude, altitude, velocity } = data;
-	//console.log(latitude);
+	//console.log(data);
 	//console.log(longitude);
+
+	marker.setLatLng([latitude, longitude]);
+	if (firstTime) {
+		mymap.setView([latitude, longitude], 2);
+		firstTime = false;
+	}
 	//display variables
-	document.getElementById('lat').textContent = latitude;
-	document.getElementById('lon').textContent = longitude;
+	document.getElementById('lat').textContent = latitude.toFixed(2);
+	document.getElementById('lon').textContent = longitude.toFixed(2);
 	document.getElementById('alt').textContent = altitude;
 	document.getElementById('vel').textContent = velocity;
 }
 
 getISS();
-
-setTimeout(function () {
-	window.location.reload(1);
-}, 5000);
+setInterval(getISS, 1500);
